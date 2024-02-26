ce9b3b222aa94e3dfe6e61554c0b111a164298a2
root <root@DESKTOP-IP51NU3>
2024-02-25 22:01:38 -0800

    routing and misc function

--- robin/central_server/central_server.py
+++ robin/central_server/central_server.py
@@ -20,7 +20,7 @@
 
 import os
 from flask import Flask, request, jsonify
-
+import requests
 ##############################################################################
 # App Creation
 app = Flask(__name__)
@@ -41,20 +41,153 @@ def request_replica():
         return vm_pool.pop(0)
     else:
         raise Exception('error: VM pool is empty')
-        
+
+# if file found, route to that node
+def route(path: str):
+    # INPUT, path
+    try:
+        for ip in nodes:
+            #check existence of file on node
+            url = ip +  '/exists/' + path
+            response = requests.get(url)
+            if response.status_code == 200:
+                return ip
+
+    # OUTPUT, corresponding node ip
+    except Exception as err:
+        return err.message
+
+
 # Global Election Counter
 ELECTION_COUNTER = 0
 
 ##############################################################################
+# Read N bytes from a path (read everything if N=-1)
+@app.route('/read/<path>/<int:position>/<int:n_bytes>', methods=['GET'])
+def read(path: str, position: int, n_bytes: int):
+    try:
+        # find route, and send request to node
+        node_ip = route(path)
+        url = f"{node_ip}/read/{path}/{position}/{n_bytes}"
+        response = requests.get(url).json()
 
-# Communicate to our server by executing GET requests to the following routes:
-#             /read/<path>/<int:position>/<int:n_bytes>
-#             /write/<path>/<data>
-#             /append/<path>/<data>
-#             /delete/<path>
-#             /copy/<src_path>/<dest_path>
-#             /rename/<old_path>/<new_path>
-#             /exists/<path>
+        # when the node responds back, forward reponse back to client
+        if response.status_code == 200:
+            new_position = response.get("position")
+            data = response.get("data")
+            return jsonify({'position': new_position, 'data': data}), 200
+        else:
+            raise Exception("Error Code " + response.status_code)
+
+    except Exception as err_msg:
+        return jsonify({'error': err_msg.message}), 400
+
+
+##############################################################################
+# Write a string to the path (creates a new file if <path> DNE)
+@app.route('/write/<path>/<data>', methods=['GET'])
+def write(path: str, data: str):
+    try:
+        # find route, and send request to node
+        node_ip = route(path)
+        url = f"{node_ip}/write/{path}/{data}"
+        response = requests.get(url)
+        # when the node responds back, forward reponse back to client
+        if response.status_code == 200:
+            return jsonify({}), 200
+        else:
+            raise Exception("Error Code " + response.status_code)
+
+    except Exception as err_msg:
+        return jsonify({'error': err_msg.message}), 400
+
+
+##############################################################################
+# Append a string to the path (creates a new file if <path> DNE)
+@app.route('/append/<path>/<data>', methods=['GET'])
+def append(path: str, data: str):
+    try:
+        # find route, and send request to node
+        node_ip = route(path)
+        url = f"{node_ip}/append/{path}/{data}"
+        response = requests.get(url)
+        # when the node responds back, forward reponse back to client
+        if response.status_code == 200:
+            return jsonify({}), 200
+        else:
+            raise Exception("Error Code " + response.status_code)
+
+    except Exception as err_msg:
+        return jsonify({'error': err_msg.message}), 400
+
+
+##############################################################################
+# Delete <path>
+@app.route('/delete/<path>', methods=['GET'])
+def delete(path: str):
+    try:
+        node_ip = route(path)
+        url = f"{node_ip}/delete/{path}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return jsonify({}), 200
+        else:
+            raise Exception("Error Code " + response.status_code)
+
+    except Exception as err_msg:
+        return jsonify({'error': err_msg.message}), 400
+
+
+##############################################################################
+# Copy <src_path> to <dest_path>
+@app.route('/copy/<src_path>/<dest_path>', methods=['GET'])
+def copy(src_path: str, dest_path: str):
+    try:
+        node_ip = route(path)
+        url = f"{node_ip}/copy/{src_path}/{dest_path}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return jsonify({}), 200
+        else:
+            raise Exception("Error Code " + response.status_code)
+
+    except Exception as err_msg:
+        return jsonify({'error': err_msg.message}), 400
+
+
+##############################################################################
+# Rename <old_path> as <new_path>
+@app.route('/rename/<old_path>/<new_path>', methods=['GET'])
+def rename(old_path: str, new_path: str):
+    try:
+        node_ip = route(path)
+        url = f"{node_ip}/delete/{path}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return jsonify({}), 200
+        else:
+            raise Exception("Error Code " + response.status_code)
+
+    except Exception as err_msg:
+        return jsonify({'error': err_msg.message}), 400
+
+
+##############################################################################
+# Rename <old_path> as <new_path> # is this needed
+@app.route('/exists/<path>', methods=['GET'])
+def exists(path: str):
+    try:
+        node_ip = route(path)
+        url = f"{node_ip}/exists/{path}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return jsonify({}), 200
+        else:
+            raise Exception("Error Code " + response.status_code)
+
+    except Exception as err_msg:
+        return jsonify({'error': err_msg.message}), 400
+
 
 
 
