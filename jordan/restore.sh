9f07912e2b09a8058675b16eabe127c9c8c7c641
Jordan Randleman <jordanran199@gmail.com>
2024-03-01 19:11:01 -0800

    Improved restore.sh
    Avoids creating a redundant temporary rile

--- jordan/restore.sh
+++ jordan/restore.sh
@@ -2,7 +2,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/jordan/ips/rvm.txt > ips_rvm_tmp.txt
-curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/jordan/ips/uvm.txt > ips_uvm_tmp.txt
-mv ./ips_rvm_tmp.txt $SCRIPT_DIR/ips/rvm.txt
-mv ./ips_uvm_tmp.txt $SCRIPT_DIR/ips/uvm.txt
+curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/jordan/ips/rvm.txt > $SCRIPT_DIR/ips/rvm.txt
+curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/jordan/ips/uvm.txt > $SCRIPT_DIR/ips/uvm.txt
