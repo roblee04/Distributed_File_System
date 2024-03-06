17e8880ce6cb95696718ccbff5022b05b78c51ae
Jordan Randleman <jordanran199@gmail.com>
2024-03-06 13:48:27 -0800

    Update restore.sh
    Improves restore.sh to also restore the pool IPs and middleware IP

--- submission/restore.sh
+++ submission/restore.sh
@@ -6,3 +6,6 @@ curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/
 curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/submission/ips/1/uvm.txt > $SCRIPT_DIR/ips/1/uvm.txt
 curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/submission/ips/2/rvm.txt > $SCRIPT_DIR/ips/2/rvm.txt
 curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/submission/ips/2/uvm.txt > $SCRIPT_DIR/ips/2/uvm.txt
+
+curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/submission/ips/middleware.txt > $SCRIPT_DIR/ips/middleware.txt
+curl -L https://raw.githubusercontent.com/roblee04/Distributed_File_System/main/submission/ips/pool-ips.txt > $SCRIPT_DIR/ips/pool-ips.txt
