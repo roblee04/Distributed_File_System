70e2e0021f4e8822ee3e9c69ab896166a4256e9c
Jordan Randleman <jordanran199@gmail.com>
2024-03-06 23:37:52 -0800

    Anchors our file operations to all take place in a certain directory
    Files are stored in "rootdir/"

---
+++ submission/rootdir/README.md
@@ -0,0 +1,3 @@
+# Root of our Distributed File System
+
+Hosts the files operated upon by our DFS application. UVMs and RVMs store their files here.
--- submission/rvm/fs.py
+++ submission/rvm/fs.py
@@ -3,25 +3,21 @@
 #   Server functionality to operate on local files.
 #   Called by an HTTP webserver process.
 
-
-
-# ############################################################################
-# @TODO: NEED TO ANCHOR ALL FILE OPERATIONS AT A SAFE ROOT (NOT JUST ANY PATH!)
-# ############################################################################
-
-
-
 # SUPPORTED APIs:
 #   1. read a file
 #   2. write data (also creates files)
-#   3. append data (also creates files)
-#   4. delete a file
-#   5. copy a file
-#   6. rename (also moves) a file
-#   7. check if a file exists
+#   3. delete a file
+#   4. copy a file
+#   5. rename (also moves) a file
+#   6. check if a file exists
 
 import os
 import shutil
+
+##############################################################################
+# Anchoring our FS operations to a certain directory
+ROOT_DIRECTORY = os.path.dirname(__file__)+'/../rootdir/'
+
 
 ##############################################################################
 # Custom Exceptions
@@ -50,7 +46,7 @@ def read(path: str, position: int, n_bytes: int = READ_ENTIRE_PATH):
     if n_bytes != READ_ENTIRE_PATH and n_bytes < 0:
         raise DistributedFileSystemError(f"read: n_bytes {n_bytes} can't be negative!")
     try:
-        with open(path, 'r') as file:
+        with open(ROOT_DIRECTORY+path, 'r') as file:
             contents = file.read()
             if n_bytes == READ_ENTIRE_PATH:
                 return len(contents), contents
@@ -63,27 +59,17 @@ def read(path: str, position: int, n_bytes: int = READ_ENTIRE_PATH):
 # Write a string to the path (creates a new file if <path> DNE)
 def write(path: str, data: str):
     try:
-        with open(path, 'w') as file:
+        with open(ROOT_DIRECTORY+path, 'w') as file:
             file.write(data)
     except Exception:
         raise DistributedFileSystemError(f"write: Path {path} can't be written!")
 
 
 ##############################################################################
-# Append a string to the path (creates a new file if <path> DNE)
-def append(path: str, data: str):
-    try:
-        with open(path, 'a') as file:
-            file.write(data)
-    except Exception:
-        raise DistributedFileSystemError(f"append: Path {path} can't be appended!")
-
-
-##############################################################################
 # Delete <path>
 def delete(path: str):
     try:
-        os.remove(path)
+        os.remove(ROOT_DIRECTORY+path)
     except Exception:
         raise DistributedFileNotFound(f"delete: Path {path} doesn't exist!")
 
@@ -92,7 +78,7 @@ def delete(path: str):
 # Copy <src_path> to <dest_path>
 def copy(src_path: str, dest_path: str):
     try:
-        shutil.copyfile(src_path,dest_path)
+        shutil.copyfile(ROOT_DIRECTORY+src_path,ROOT_DIRECTORY+dest_path)
     except Exception as e:
         raise DistributedFileNotFound(f"copy: Path {src_path} doesn't exist!")
 
@@ -101,7 +87,7 @@ def copy(src_path: str, dest_path: str):
 # Rename <old_path> as <new_path>
 def rename(old_path: str, new_path: str):
     try:
-        os.rename(old_path, new_path)
+        os.rename(ROOT_DIRECTORY+old_path,ROOT_DIRECTORY+new_path)
     except Exception as e:
         raise DistributedFileNotFound(f"rename: Path {old_path} doesn't exist!")
 
@@ -109,4 +95,4 @@ def rename(old_path: str, new_path: str):
 ##############################################################################
 # Check if <path> exists
 def exists(path: str) -> bool:
-    return os.path.exists(path)
+    return os.path.exists(ROOT_DIRECTORY+path)
